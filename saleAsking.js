8037caa4b6fcfe57365c38258ae71dc9fcd9ae90
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 20:09:27 +0700

    m

--- saleAsking.js
+++ saleAsking.js
@@ -10,7 +10,7 @@ module.exports = function (ZOAClient, userId, data, responseAI) {
         }
 
         var keyword = responseAI.name[0].value;
-        data.find(f=> console.log(f.name));
+        data.find(f=> console.log(f.name.toLowerCase()));
         var isExisted = data.find(f => f.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1);
         console.log(keyword);
         if (isExisted) {
