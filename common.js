ceb601c170798d240fad8b1472085f4f58686f41
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 15:37:54 +0700

    m

--- common.js
+++ common.js
@@ -1,80 +1,80 @@
-module.exports = function(ZOAClient) {
+module.exports = function (ZOAClient) {
     var module = {};
 
-    module.sendTextMessage = function(userId, message) {
+    module.sendTextMessage = function (userId, message) {
         ZOAClient.api('sendmessage/text', 'POST', { uid: userId, message: message }, function (response) {
-          console.log(response);
+            console.log(response);
         });
-      }
-      
-      module.sendImageMessage = function(userId, message, imageId) {
+    }
+
+    module.sendImageMessage = function (userId, message, imageId) {
         ZOAClient.api('sendmessage/image', 'POST', { uid: userId, message: message, imageid: imageId }, function (response) {
-          console.log(response);
+            console.log(response);
         });
-      }
-      
-    //   function sendLinkMessage(userId, links) {
-    //     var params = {
-    //       uid: userId,
-    //       links: links
-    //     };
-      
-    //     ZOAClient.api('sendmessage/links', 'POST', params, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function sendInteractionMessage(userId, actions) {
-    //     var params = {
-    //       uid: userId,
-    //       actionlist: actions
-    //     };
-      
-    //     ZOAClient.api('sendmessage/actionlist', 'POST', params, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function sendSMSMessage(phoneNo, templateId, templateData) {
-    //     var params = {
-    //       phone: phoneNo,
-    //       templateid: templateId,
-    //       templatedata: {}
-    //     };
-      
-    //     ZOAClient.api('sendmessage/phone/cs', 'POST', params, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function sendCustomerCareMessage(userId, templateId, templateData) {
-    //     var params = {
-    //       uid: userId,
-    //       templateid: templateId,
-    //       templatedata: {}
-    //     };
-      
-    //     ZOAClient.api('sendmessage/cs', 'POST', params, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function replyTextMessage(messageId, message) {
-    //     ZOAClient.api('sendmessage/reply/text', 'POST', { msgid: messageId, message: message }, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function replyImageMessage(messageId, imageId, message) {
-    //     ZOAClient.api('sendmessage/reply/image', 'POST', { msgid: messageId, imageid: imageId, message: message }, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
-      
-    //   function replyLinkMessage(messageId, link) {
-    //     ZOAClient.api('sendmessage/reply/links', 'POST', { msgid: messageId, links: link }, function (response) {
-    //       console.log(response);
-    //     });
-    //   }
+    }
+
+    module.sendLinkMessage = function (userId, links) {
+        var params = {
+            uid: userId,
+            links: links
+        };
+
+        ZOAClient.api('sendmessage/links', 'POST', params, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.sendInteractionMessage = function (userId, actions) {
+        var params = {
+            uid: userId,
+            actionlist: actions
+        };
+
+        ZOAClient.api('sendmessage/actionlist', 'POST', params, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.sendSMSMessage = function (phoneNo, templateId, templateData) {
+        var params = {
+            phone: phoneNo,
+            templateid: templateId,
+            templatedata: {}
+        };
+
+        ZOAClient.api('sendmessage/phone/cs', 'POST', params, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.sendCustomerCareMessage = function (userId, templateId, templateData) {
+        var params = {
+            uid: userId,
+            templateid: templateId,
+            templatedata: {}
+        };
+
+        ZOAClient.api('sendmessage/cs', 'POST', params, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.replyTextMessage = function (messageId, message) {
+        ZOAClient.api('sendmessage/reply/text', 'POST', { msgid: messageId, message: message }, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.replyImageMessage = function (messageId, imageId, message) {
+        ZOAClient.api('sendmessage/reply/image', 'POST', { msgid: messageId, imageid: imageId, message: message }, function (response) {
+            console.log(response);
+        });
+    }
+
+    module.replyLinkMessage = function (messageId, link) {
+        ZOAClient.api('sendmessage/reply/links', 'POST', { msgid: messageId, links: link }, function (response) {
+            console.log(response);
+        });
+    }
     return module;
 }
