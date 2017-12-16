7e58c90b147bebad1c399f7611d492d0f53bd0a5
Ninh <levanninh0101@gmail.com>
2017-12-16 15:39:27 +0000

    finish fullTechInfoAsking

---
+++ fullTechInfoAsking.js
@@ -0,0 +1,30 @@
+module.exports = function(ZOAClient, userId , data, responseAI){
+    var module = {};
+    var common = require('./common.js')(ZOAClient);
+        
+    module.execute  = function () {
+        var keyword = responseAI.name[0].value.toLowerCase();
+        var product = null;
+        for(let i = 0 ; i < data.length ; i++) {
+            if(data[i].name) {
+                var productName = data[i].name.toLowerCase();  
+                if(productName.indexOf(keyword) !== -1) {
+                    product = data[i];
+                    break;
+                }
+            }
+        }
+        if(product) {
+            var stringData = JSON.stringify(product.fullTechInfo);
+            var stringResponse = stringData.replace(/[{}]/g, '\n');
+            stringResponse = stringResponse.replace(/\",/g, '\n');
+            stringResponse = stringResponse.replace(/\"/g, '')
+            common.sendTextMessage(userId ,  stringResponse); 
+        }
+        else {
+             common.sendTextMessage(userId, "Rất tiếc. Không có thông tin kĩ thuật của sản phẩm."); 
+        }
+    }
+    
+    return module;
+}
