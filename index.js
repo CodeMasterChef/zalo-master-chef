bea39e90e5726aa5dd0ec5b9a5943fdc9603012d
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 18:13:25 +0700

    m

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ server.get('/webhook', (req, res) => {
 
     request(options, function (error, response, body) {
       var data = JSON.parse(body).entities;
-      console.log(Object.keys(data)[0]);
+      console.log(Object.keys(data));
       switch (Object.keys(data)[0]) {
         case 'greetingAsking':
           common.sendTextMessage(userId, 'Xin chào, ' + username);
