5575e5a21470ac319456de704a6597484f54743b
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 10:39:37 +0700

    Send as interaction

--- index.js
+++ index.js
@@ -44,32 +44,46 @@ server.get('/webhook', (req, res) => {
   });
 
   ZOAClient.api('sendmessage/image', 'POST', {
-    uid: userId, 
-    message: 'Zalo SDK Nodejs', 
+    uid: userId,
+    message: 'Zalo SDK Nodejs',
     'imageid': '57-134185-combo-sac-du-phong-10000-mah-cap-micro-usb-20-cm-a-18-300x300.jpg'
-  }, function(response) {
+  }, function (response) {
     console.log(response);
   });
 
   var params = {
     uid: userId,
     actionlist: [{
-        action: 'oa.open.inapp',
-        title: 'Send interactive messages',
-        description: 'This is a test for API send interactive messages',
-        thumb: 'https://developers.zalo.me/web/static/images/bg.jpg',
-        href: 'https://developers.zalo.me',
-        data: 'https://developers.zalo.me',
-        popup: {
-            title: 'Open Website Zalo For Developers',
-            desc: 'Click ok to visit Zalo For Developers and read more Document',
-            ok: 'ok',
-            cancel: 'cancel'
-        }
+      action: 'oa.open.inapp',
+      title: 'Send interactive messages',
+      description: 'This is a test for API send interactive messages',
+      thumb: 'https://developers.zalo.me/web/static/images/bg.jpg',
+      href: 'https://developers.zalo.me',
+      data: 'https://developers.zalo.me',
+      popup: {
+        title: 'Open Website Zalo For Developers',
+        desc: 'Click ok to visit Zalo For Developers and read more Document',
+        ok: 'ok',
+        cancel: 'cancel'
+      }
     }]
   }
-  ZOAClient.api('sendmessage/actionlist', 'POST', params, function(response) {
-      console.log(response);
+  ZOAClient.api('sendmessage/actionlist', 'POST', params, function (response) {
+    console.log(response);
+  });
+
+  ZSClient.api('me/friends', function (response) {
+    console.log(response);
+  });
+
+  //Get list of your friends with offset and limit
+  ZSClient.api('me/friends', { offset: 10, limit: 50 }, function (response) {
+    console.log(response);
+  });
+
+  //Get  list of your friends who you can intvite to use your App.
+  ZSClient.api('me/invitable_friends', { offset: 10, limit: 50, fields: 'id, name, picture' }, function (response) {
+    console.log(response);
   });
 })
 
