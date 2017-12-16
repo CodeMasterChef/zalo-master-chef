5c7063ead061e5f7649163914ced433295b80a01
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 10:21:25 +0700

    Send as picture

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ server.get('/webhook', (req, res) => {
   ZOAClient.api('sendmessage/image', 'POST', {
     uid: userId, 
     message: 'Zalo SDK Nodejs', 
-    imageid: '57-134185-combo-sac-du-phong-10000-mah-cap-micro-usb-20-cm-a-18-300x300.jpg'
+    'imageid': '57-134185-combo-sac-du-phong-10000-mah-cap-micro-usb-20-cm-a-18-300x300.jpg'
   }, function(response) {
     console.log(response);
   });
