fc537e155bb753b1c86dbad1f8c25671c044f141
Khanh Nguyen <khanh.nguyen6694@gmail.com>
2017-12-16 19:01:05 +0700

    m

--- saleAsking.js
+++ saleAsking.js
@@ -2,7 +2,7 @@ module.exports = function(ZOAClient, data, responseAI){
     var module = {};
     var common = require('./common.js')(ZOAClient);
 
-    console.log(data);
+    console.log(responseAI);
 
     return module;
 }
